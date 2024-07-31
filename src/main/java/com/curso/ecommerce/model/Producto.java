e650780985844fb2ae33a22094f6094836c7ff98
Cristopher Placido Ocaña <cristopher.placido@cordonbleu.edu.pe>
2024-07-30 20:16:38 -0500

    Crear las clases model, mapeo clases a tablas con JPA, integrado la parte de diseño y vistas

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,6 +1,13 @@
 package com.curso.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String descripcion;
@@ -8,7 +15,20 @@ public class Producto {
     private Double precio;
     private int cantidad;
 
+    @ManyToOne
+    private Usuario usuario;
+
     public Producto() {
+    }
+
+    public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, int cantidad, Usuario usuario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.usuario = usuario;
     }
 
     public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, int cantidad) {
@@ -66,6 +86,14 @@ public class Producto {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     @Override
